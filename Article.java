83eb9f4d47a78424156014acb80c1ffe3d941f1d
yountaeseop <jangmj6931@gmail.com>
2021-11-11 23:07:55 +0900

    게시물 수정 삭제기능

---
+++ Article.java
@@ -0,0 +1,15 @@
+package Board;
+
+public class Article {
+
+	int id;
+	String title;
+	String content;
+	
+//	public Artile(int id, String title, String content) {
+//		this.id = id;
+//		this.title = title;
+//		this.content = content;
+//	} -> 한번에 하는 방법있음
+	
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+package Board;
+
+public class Main {
+
+	public static void main(String[] args) {
+		board myBoard = new board();
+		myBoard.runBoard();
+
+	}
+
+}
