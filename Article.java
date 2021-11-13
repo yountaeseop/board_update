e2f6a9adb677b438576524774e9603dbca73d02c
yountaeseop <jangmj6931@gmail.com>
2021-11-13 20:16:47 +0900

    아티클 생성자 만들기

--- Article.java
+++ Article.java
@@ -6,10 +6,16 @@ public class Article {
 	String title;
 	String content;
 	
+	public Article(int id, String title, String content) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.content = content;
+	}
 //	public Artile(int id, String title, String content) {
 //		this.id = id;
 //		this.title = title;
 //		this.content = content;
-//	} -> 한번에 하는 방법있음
+//	} -> 한번에 하는 방법있음    -> Source -> Generate constructor using Fields -> Generate
 	
 }
