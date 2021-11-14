41e4f6885571f00d79fc4277025b96bb167db970
yountaeseop <jangmj6931@gmail.com>
2021-11-15 01:14:29 +0900

    함수적용 정리

--- board.java
+++ board.java
@@ -19,80 +19,97 @@ public class board {
 			String order = sc.nextLine();
 			
 			if(order.equals("help")) {
-				System.out.println("add : 게시물 등록");
-				System.out.println("list : 게시물 목록 조회");
-				System.out.println("update : 게시물 수정");
-				System.out.println("delete : 게시물 삭제");
-				System.out.println("search : 게시물 검색");
-			}
-			else if(order.equals("add")) {
-				
-				
-				System.out.print("제목을 입력해주세요:");
-				String title = sc.nextLine();
-				
-				System.out.print("내용을 입력해주세요:");
-				String content = sc.nextLine();
-				
-				Article Article = new Article(num, title, content);
-				Articles.add(Article);
-			
-				num++; // 게시물 등록번호 자동증가
-				
-				System.out.println("게시물이 저장되었습니다.");
-			}
-			else if(order.equals("list")) {
+				printHelp();
+			}else if(order.equals("add")) {
+				addArticle();
+			}else if(order.equals("list")) {
 				list();
+			}else if(order.equals("update")) {
+				updateArticle(); 
+			}else if(order.equals("delete")){
+				deleteArticle();
+			}else if(order.equals("search")) {
+				searchArticle();
 			}
-			else if(order.equals("update")) {
-				System.out.print("수정할 게시물 번호:");
-				int targetNum = Integer.parseInt(sc.nextLine());
-				
-				int standard = standard(targetNum);
-				// standard의 값을 초기화시키는 반복문 -> 거름망 역할을 함
-				
-				if(standard == -1) {
-					System.out.println("없는 게시물입니다.");
-				} else {
-					System.out.print("제목:");
-					String title = sc.nextLine();
-					System.out.print("내용:");
-					String content = sc.nextLine();
-					
-					
-					Article  Article = new Article(targetNum, title, content);
-					Articles.set(standard, Article);
-					
-					
-					System.out.println("수정이 완료되었습니다.");
-				}
-				
-				
-				list();
-			}
-			else if(order.equals("delete")){
-				System.out.println("삭제할 게시물 번호:");
-				int targetNum = Integer.parseInt(sc.nextLine());
-				
-				int standard = standard(targetNum);
-			
-				if(standard == -1) {
-					System.out.println("없는 게시물 번호 입니다.");
-				} else {
-					
-					Articles.remove(standard);
-					
-					System.out.println("삭제가 완료되었습니다.");
-					
-					list();
-				}
-				
-			}
-			
-			
 			System.out.println("================");
+		}	
+	}
+
+	private void searchArticle() {
+		
+		
+	}
+
+	private void deleteArticle() {
+		System.out.println("삭제할 게시물 번호:");
+		int targetNum = Integer.parseInt(sc.nextLine());
+		
+		int standard = standard(targetNum);
+	
+		if(standard == -1) {
+			System.out.println("없는 게시물 번호 입니다.");
+		} else {
+			
+			Articles.remove(standard);
+			
+			System.out.println("삭제가 완료되었습니다.");
+			
+			list();
 		}
 		
+		
+	}
+
+	private void updateArticle() {
+		System.out.print("수정할 게시물 번호:");
+		int targetNum = Integer.parseInt(sc.nextLine());
+		
+		int standard = standard(targetNum);
+		// standard의 값을 초기화시키는 반복문 -> 거름망 역할을 함
+		
+		if(standard == -1) {
+			System.out.println("없는 게시물입니다.");
+		} else {
+			System.out.print("제목:");
+			String title = sc.nextLine();
+			System.out.print("내용:");
+			String content = sc.nextLine();
+			
+			
+			Article  Article = new Article(targetNum, title, content);
+			Articles.set(standard, Article);
+			
+			
+			System.out.println("수정이 완료되었습니다.");
+		}
+		
+		
+		list();
+		
+	}
+
+	private void addArticle() {
+		System.out.print("제목을 입력해주세요:");
+		String title = sc.nextLine();
+		
+		System.out.print("내용을 입력해주세요:");
+		String content = sc.nextLine();
+		
+		Article Article = new Article(num, title, content);
+		Articles.add(Article);
+	
+		num++; // 게시물 등록번호 자동증가
+		
+		System.out.println("게시물이 저장되었습니다.");
+		
+	}
+
+	private void printHelp() {
+		System.out.println("add : 게시물 등록");
+		System.out.println("list : 게시물 목록 조회");
+		System.out.println("update : 게시물 수정");
+		System.out.println("delete : 게시물 삭제");
+		System.out.println("search : 게시물 검색");
 		
 	}
 
